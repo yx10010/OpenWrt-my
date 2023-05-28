00b4d148c207c40e40e33c0f5045fd130c6d33fa
TIAmo <130211313+ysx88@users.noreply.github.com>
2023-05-28 19:27:54 +0800

    Update diy-x86.sh

--- config/diy-x86.sh
+++ config/diy-x86.sh
@@ -29,6 +29,5 @@ sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/M
 # 整理固件包时候,删除您不想要的固件或者文件,让它不需要上传到Actions空间(根据编译机型变化,自行调整删除名称)
 cat >"$CLEAR_PATH" <<-EOF
 IPK.tar.gz
-openwrt-x86-64-generic-kernel.bin
 profiles.json
 EOF
