7b2f3e4cdaa12ec999e01c16683a0720b7d0e65d
TIAmo <130211313+ysx88@users.noreply.github.com>
2023-06-03 06:27:39 +0800

    Add files via upload

---
+++ configs/diy-n1.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+#
+# Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
+#
+# This is free software, licensed under the MIT License.
+# See /LICENSE for more information.
+#
+# https://github.com/P3TERX/Actions-OpenWrt
+# File name: diy-part2.sh
+# Description: OpenWrt DIY script part 2 (After Update feeds)
+#
+
+# 修改默认IP
+#sed -i 's/192.168.1.1/192.168.71.4/g' package/base-files/files/bin/config_generate
+
+# 移除重复软件包
+rm -rf feeds/luci/themes/luci-theme-argon
+
+# Themes
+git clone --depth 1 -b 18.06 https://github.com/kiddin9/luci-theme-edge package/luci-theme-edge
+git clone --depth 1 -b 18.06 https://github.com/jerrykuku/luci-theme-argon package/luci-theme-argon
+git clone --depth 1 https://github.com/jerrykuku/luci-app-argon-config package/luci-app-argon-config
+
+#取消原主题luci-theme-bootstrap为默认主题
+sed -i '/set luci.main.mediaurlbase=\/luci-static\/bootstrap/d' feeds/luci/themes/luci-theme-bootstrap/root/etc/uci-defaults/30_luci-theme-bootstrap
+
+# 修改 argon 为默认主题,可根据你喜欢的修改成其他的（不选择那些会自动改变为默认主题的主题才有效果）
+sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
---
+++ configs/diy-x86.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+
+# Modify default IP
+#sed -i 's/192.168.1.1/192.168.88.2/g' package/base-files/files/bin/config_generate
+
+sed -i 's/os.date()/os.date("%Y-%m-%d %H:%M:%S")/g' package/lean/autocore/files/arm/index.htm
+
+#删除原默认主题
+rm -rf feeds/luci/themes/luci-theme-argon
+rm -rf feeds/small8/luci-theme-argon
+rm -rf feeds/small8/luci-app-argon-config
+#rm -rf feeds/luci/luci-theme-bootstrap
+#rm -rf feeds/luci/luci-theme-material
+#rm -rf feeds/luci/luci-theme-netgear
+#rm -rf feeds/luci/luci-theme-ifit
+#rm -rf feeds/luci/luci-theme-neobird
+
+#下载主题luci-theme-argon
+git clone --depth=1 -b 18.06 https://github.com/jerrykuku/luci-theme-argon package/lean/luci-theme-argon
+git clone --depth=1 https://github.com/jerrykuku/luci-app-argon-config package/lean/luci-app-argon-config
+
+#取消原主题luci-theme-bootstrap为默认主题
+sed -i '/set luci.main.mediaurlbase=\/luci-static\/bootstrap/d' feeds/luci/themes/luci-theme-bootstrap/root/etc/uci-defaults/30_luci-theme-bootstrap
+
+# 修改 argon 为默认主题,可根据你喜欢的修改成其他的（不选择那些会自动改变为默认主题的主题才有效果）
+sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
