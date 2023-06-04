552b60dcfbf50c6c6648a70487f5d91277e0ed0b
TIAmo <130211313+ysx88@users.noreply.github.com>
2023-06-04 14:08:04 +0800

    Update diy-part1.sh

--- config/diy-part1.sh
+++ config/diy-part1.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
-#===============================================
-# Description: DIY script part 1
+#
+# Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
+#
+# This is free software, licensed under the MIT License.
+# See /LICENSE for more information.
+#
+# https://github.com/P3TERX/Actions-OpenWrt
 # File name: diy-part1.sh
-# Lisence: MIT
-# By: Jejz
-#===============================================
+# Description: OpenWrt DIY script part 1 (Before Update feeds)
+#
 
+# Uncomment a feed source
+#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-sed -i "/helloworld/d" feeds.conf.default
-sed -i '$a src-git Jejz https://github.com/Jejz168/openwrt-packages' feeds.conf.default
-# sed -i '$a src-git helloworld https://github.com/fw876/helloworld' feeds.conf.default
-# sed -i '$a src-git PWpackages https://github.com/xiaorouji/openwrt-passwall.git;packages' feeds.conf.default
+#echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
+#echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
