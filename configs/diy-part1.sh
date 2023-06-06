1ed1eaa6787b62d12eadfe111b6d7355cab34289
TIAmo <130211313+ysx88@users.noreply.github.com>
2023-06-06 18:22:36 +0800

    Update diy-part1.sh

--- configs/diy-part1.sh
+++ configs/diy-part1.sh
@@ -15,5 +15,6 @@
 
 # Add a feed source
 echo 'src-git ysx88 https://github.com/ysx88/my-packages' >>feeds.conf.default
+echo 'src-git ysx88 https://github.com/ysx88/openwrt-packages' >>feeds.conf.default
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
