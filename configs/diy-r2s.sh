12897ef597ec5b195542f710ea2a55a3eb02117d
TIAmo <130211313+ysx88@users.noreply.github.com>
2023-06-07 16:27:57 +0800

    Update diy-r2s.sh

--- configs/diy-r2s.sh
+++ configs/diy-r2s.sh
@@ -4,12 +4,13 @@
 # 修改openwrt登陆地址,把下面的192.168.11.1修改成你想要的就可以了
 sed -i 's/192.168.1.1/192.168.71.3/g' package/base-files/files/bin/config_generate
 
-#删除原默认主题
+# 移除重复软件包
 rm -rf feeds/luci/themes/luci-theme-argon
 
-#下载主题luci-theme-argon
-git clone --depth=1 -b 18.06 https://github.com/jerrykuku/luci-theme-argon package/lean/luci-theme-argon
-git clone --depth=1 https://github.com/jerrykuku/luci-app-argon-config package/lean/luci-app-argon-config
+# Themes
+git clone --depth 1 -b 18.06 https://github.com/kiddin9/luci-theme-edge package/luci-theme-edge
+git clone --depth 1 -b 18.06 https://github.com/jerrykuku/luci-theme-argon package/luci-theme-argon
+git clone --depth 1 https://github.com/jerrykuku/luci-app-argon-config package/luci-app-argon-config
 
 #取消原主题luci-theme-bootstrap为默认主题
 sed -i '/set luci.main.mediaurlbase=\/luci-static\/bootstrap/d' feeds/luci/themes/luci-theme-bootstrap/root/etc/uci-defaults/30_luci-theme-bootstrap
